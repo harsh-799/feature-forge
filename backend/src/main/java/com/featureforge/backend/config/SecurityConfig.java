c34ab701219a20843bf40d9dd7bbfbc745dbe08c
harsh-799 <harshanand7898@gmail.com>
2026-07-22 23:41:53 +0530

    backend: disable default basic auth of spring security

---
+++ backend/src/main/java/com/featureforge/backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.featureforge.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable());
+        http.httpBasic(basic -> basic.disable());
+        http.formLogin(formLogin -> formLogin.disable());
+
+        return http.build();
+    }
+
+}

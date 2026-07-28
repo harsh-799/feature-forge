10df6a3b070c5953b835bf9aa1050d309e9ba7b5
harsh-799 <harshanand7898@gmail.com>
2026-07-28 12:35:53 +0530

    backend: show response for JWT related exceptions

--- backend/src/main/java/com/featureforge/backend/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/featureforge/backend/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,9 @@ package com.featureforge.backend.filter;
 
 import com.featureforge.backend.service.CustomUserDetailsService;
 import com.featureforge.backend.service.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
@@ -32,20 +36,49 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String token = authorizationHeader.substring("Bearer ".length());
 
-        if (jwtService.validateJWTToken(token)) {
+        try {
+            // Authentication is implemented with JWT for now.
+            // Code structured to support additional auth strategies in the future (e.g., OAuth2).
 
-            String userEmail = jwtService.extractUsername(token);
-            UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.validateJWTToken(token) && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-            Authentication authentication = new UsernamePasswordAuthenticationToken(
-                    userDetails,
-                    null,
-                    userDetails.getAuthorities()
-            );
+                String userEmail = jwtService.extractUsername(token);
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (UsernameNotFoundException e) {
+            sendResponse(response, "USER_NOT_FOUND","User not found");
+            return;
+        } catch (ExpiredJwtException e) {
+            sendResponse(response, "TOKEN_EXPIRED","Token Expired");
+            return;
+        } catch (JwtException e) {
+            sendResponse(response, "INVALID_TOKEN", "Invalid token");
+            return;
         }
 
         filterChain.doFilter(request,response);
+    }
+
+    private void sendResponse(HttpServletResponse response, String code, String message) throws IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        String body = """
+        {
+            "success": false,
+            "code": "%s",
+            "message": "%s"
+        }
+        """.formatted(code,message);
+
+        response.getWriter().write(body);
     }
 }
